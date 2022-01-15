9e7a6651fd544498b893725a255dfffba6a02b3d
zxk <524935836@qq.com>
2022-01-15 20:56:48 +0800

    初始化购物车组件

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Register from '@/views/Register'
 import Search from '@/views/Search'
 import Detail from '@/views/Detail'
 import AddCartSuccess from '@/views/AddCartSuccess'
+import ShopCart from '@/views/ShopCart'
 
 const originPush = VueRouter.prototype.push
 const originReplace = VueRouter.prototype.replace
@@ -35,8 +36,9 @@ const routes = [
   { name: 'search', path: '/search/:keyword?', component: Search, meta: { show: true } },
   { name: 'login', path: '/login', component: Login, meta: { show: false } },
   { name: 'register', path: '/register', component: Register, meta: { show: false } },
-  { name: 'detail', path: '/detail/:skuId ', component: Detail, meta: { show: true } },
-  { name: 'addCartSuccess', path: '/addCartSuccess', component: AddCartSuccess, meta: { show: true } }
+  { name: 'detail', path: '/detail/:skuId', component: Detail, meta: { show: true } },
+  { name: 'addCartSuccess', path: '/addCartSuccess', component: AddCartSuccess, meta: { show: true } },
+  { name: 'shopCart', path: '/shopCart', component: ShopCart, meta: { show: true } }
 ]
 
 const router = new VueRouter({
